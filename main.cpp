c08506e736e0564097be1ab7bc6fe282d3b2fcce
ExlicirS <44773816+ExlicirS@users.noreply.github.com>
2018-11-16 13:17:32 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "ChainFxs.h" //Chains of Functions
+ChainFxs ChainFxs; //Instantiate an object to call the functions of ChainFxs.cpp, a must before calling class members
+
+
+int main() //Main entry point of the application
+{
+    ChainFxs.MultiplyAndPrintsBack(7, 8);
+}
+
