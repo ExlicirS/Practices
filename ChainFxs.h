41bff338bdb2083b7aee4e16f10642b548f45a7e
ExlicirS <44773816+ExlicirS@users.noreply.github.com>
2018-11-16 13:31:45 +0800

    Header file for "ChainFxs.h"

---
+++ ChainFxs.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+class ChainFxs
+{
+    public:
+        ChainFxs();
+        int Multiply(int a, int b);
+        void MultiplyAndPrintsBack (int a, int b);
+
+
+    private:
+};
