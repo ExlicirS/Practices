b1d2c99b544a6ab5665a65b154de95b90707a22d
ExlicirS <44773816+ExlicirS@users.noreply.github.com>
2018-11-16 13:41:41 +0800

    Update and rename main.cpp to ChainFxs/main.cpp

--- ChainFxs/main.cpp
+++ ChainFxs/main.cpp
@@ -6,10 +6,7 @@ ChainFxs ChainFxs; //Instantiate an object to call the functions of ChainFxs.cpp
 
 int main() //Main entry point of the application
 {
-    /*
     ChainFxs.MultiplyAndPrintsBack(7, 8); //A very shorterned way to call multiple functions
     ChainFxs.MultiplyAndPrintsBack(5, 2);
     ChainFxs.MultiplyAndPrintsBack(9, 6);
-    */
 }
-
